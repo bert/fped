069e82ffc4615aeb33ea97b08daa40518f9ae9e5
Xiangfu Liu <xiangfu@sharism.cc>
2010-10-13 00:33:31 +0800

    use usual name for orig tarball top-level directory
    Signed-off-by: Xiangfu Liu <xiangfu@sharism.cc>

--- debian/get-orig-source.sh
+++ debian/get-orig-source.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 # Build a tarball from the latest upstream version, with a nice
 # version number.
-#
-# Requires git 1.6.6 or later, GNU date, and gzip.
 
 set -e
-
-trap 'rm -fr debian-orig-source || exit 1' EXIT INT TERM
-
-svn export -r${REV} http://svn.openmoko.org/trunk/eda/fped debian-orig-source
 
 # Determine version number.
 release=0.0
 upstream_version="${release}+r${REV}"
 
+TOPFOLDER=fped-$upstream_version.orig
+
+trap 'rm -fr ${TOPFOLDER} || exit 1' EXIT INT TERM
+
+svn export -r${REV} http://svn.openmoko.org/trunk/eda/fped ${TOPFOLDER}
+
 # Generate tarball.
 echo "packaging ..."
-tar -czf fped_$upstream_version.orig.tar.gz debian-orig-source
+tar -czf fped_$upstream_version.orig.tar.gz ${TOPFOLDER}
